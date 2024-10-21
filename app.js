5214049b1e5066d15a7c0dde949e461670f3f9ad
kajunuts <tokaju1123@gmail.com>
2024-10-21 16:15:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ connectButton.addEventListener('click', async () => {
     // デバイスを検索
     const device = await navigator.bluetooth.requestDevice({
       acceptAllDevices: true,
-      optionalServices: ['beb5483e-36e1-4688-b7f5-ea07361b26a8']
+      optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b']
     });
 
     // GATTサーバに接続
